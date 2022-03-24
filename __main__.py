b97527123e8d8f0f3c4da711b5427c8b3016fa91
Kade Wilson <wil21131@byui.edu>
2022-03-24 12:38:07 -0600

    Changed some file names.
    I also added the base code for director, main, and constants. Some of the files in our game plan doc weren't what they should've been, so I renamed a few and added cast into casting.

---
+++ __main__.py
@@ -0,0 +1,42 @@
+import constants
+
+from asteroids.casting.cast import Cast
+from asteroids.casting.beam import Beam
+from asteroids.casting.score import Score
+from asteroids.casting.ship import Ship
+from asteroids.scripting.script import Script
+from asteroids.scripting.control_ship_action import ControlActorsAction
+from asteroids.scripting.move_actors_action import MoveActorsAction
+from asteroids.scripting.handle_collisions_action import HandleCollisionsAction
+from asteroids.scripting.draw_actors_action import DrawActorsAction
+from asteroids.director import Director
+from asteroids.services.keyboard_service import KeyboardService
+from asteroids.services.video_service import VideoService
+from asteroids.shared.color import Color
+from asteroids.shared.point import Point
+
+
+def main():
+    
+    # create the cast
+    cast = Cast()
+    cast.add_actor("beams", Beam())
+    cast.add_actor("ships", Ship("player1"))
+    cast.add_actor("scores", Score("player1"))
+   
+    # start the game
+    keyboard_service = KeyboardService()
+    video_service = VideoService()
+
+    script = Script()
+    script.add_action("input", ControlActorsAction(keyboard_service))
+    script.add_action("update", MoveActorsAction())
+    script.add_action("update", HandleCollisionsAction())
+    script.add_action("output", DrawActorsAction(video_service))
+    
+    director = Director(video_service)
+    director.start_game(cast, script)
+
+
+if __name__ == "__main__":
+    main()
