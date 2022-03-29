aca9d826e9ca1699b9e2294baaa89693e2c674fc
Eric Costa <ericdevcosta@users.noreply.github.com>
2022-03-29 08:50:43 -0300

    Adding actor, cast, ship, action, color, and point.

---
+++ asteroids/casting/ship.py
@@ -0,0 +1,14 @@
+from distutils.command.build_scripts import first_line_re
+import constants
+from asteroids.casting.actor import Actor
+from asteroids.shared.point import Point
+
+class ship(Actor):
+    """
+    
+    
+    The responsibility of rider is to move itself.
+
+    Attributes:
+        _points (int): The number of points for destroing asteroids.
+    """
--- asteroids/scripting/action.py
+++ asteroids/scripting/action.py
@@ -1,2 +1,18 @@
 
-#This class starts the "execute" function which all other "actions" will inherit and polymorph from  
+#This class starts the "execute" function which all other "actions" will inherit and polymorph from 
+class Action:
+    """A thing that is done.
+    
+    The responsibility of action is to do somthing that is integral or important in the game. Thus,
+    it has one method, execute(), which should be overridden by derived classes.
+    """
+
+    def execute(self, cast, script):
+        """Executes something that is important in the game. This method should be overriden by 
+        derived classes.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        pass
