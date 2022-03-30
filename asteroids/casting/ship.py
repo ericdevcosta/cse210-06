b1291bb50f655beacb00abe4d8df0c0cc756f2c9
Eric Costa <ericdevcosta@users.noreply.github.com>
2022-03-29 23:40:47 -0300

    Update ship.py

--- asteroids/casting/ship.py
+++ asteroids/casting/ship.py
@@ -6,8 +6,19 @@ class ship(Actor):
     """
     
     
-    The responsibility of rider is to move itself.
+    The responsibility of ship is to move itself and shoot asteroids.
 
     Attributes:
         _points (int): The number of points for destroing asteroids.
     """
+    def __init__(self):
+        super().__init__()
+
+    def turn_ship(self, velocity):
+        pass
+
+    def move_ship(self, velocity):
+        pass
+
+    def shoot_beam(self,velocity):
+        pass
