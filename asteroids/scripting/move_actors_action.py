22d4fc3293ae5f7f82a7556ac828e1fe72c1cfd1
Noah Blevins <37628172+Noah-Blevins@users.noreply.github.com>
2022-03-31 15:38:16 -0600

    Added base actions
    Hopefully things work? Right now we have import errors in places and that's the main thing stopping us from seeing any logic errors.

--- asteroids/scripting/move_actors_action.py
+++ asteroids/scripting/move_actors_action.py
@@ -1,2 +1,14 @@
 
 #This class should inherit action.py and polymorph the execute function
+
+from asteroids.scripting.action import Action
+
+class MoveActorsAction(Action):
+
+    def execute(self, cast, script):
+        
+        actors = cast.get_all_actors()
+
+        for actor in actors:
+            actor.move_next()
+            
